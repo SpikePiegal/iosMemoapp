4515eebaac384bad9487923a2c1f9807e32fc6a0
Cistudent <cistudent@CISs-Mac.local>
2019-04-26 10:03:07 -0700

    Created project

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Cistudent on 4/26/19.
+//  Copyright © 2019 Learning Mobile Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
