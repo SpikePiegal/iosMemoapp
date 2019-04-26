008d23e0af6311d3a068236b97eb0dd9fb270428
Cistudent <cistudent@CISs-Mac.local>
2019-04-26 13:45:04 -0700

    updated core data

--- Notes/Notes/AppDelegate.swift
+++ Notes/Notes/AppDelegate.swift
@@ -16,6 +16,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+      //  UINavigationBar.appearance().barTintColor = UIColor(red: 204.0/255.0, green: 204.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+   //     UINavigationBar.appearance().tintColor = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 242.0/255.0, alpha: 1.0)
+        
+    //    let color = UIColor(red: 204.0/255.0, green: 204.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+
+        //let font = UIFont(name:)
+        
+   //      let attributes : [NSAttributedStringKey: AnyObject] = [
+   //          NSAttributedStringKey.foregroundColor: color
+ // ]
+        
+    //    UINavigationBar.appearance().titleTextAttributes = attributes
+   //     UIApplication.shared.statusBarStyle = .lightContent
+        
         // Override point for customization after application launch.
         return true
     }
